59874c83640aea8b73c75a0e27d73a87c7b7cdd7
Winterflower <camillamon@gmail.com>
2015-03-07 21:33:34 +0000

    Changes to mainapp

--- mainapp.py
+++ mainapp.py
@@ -30,9 +30,54 @@ def before_request():
     g.db=connect_db()
 
 
-@app.after_request
-def after_request():
-    print "Request completed"
+@app.teardown_request
+def teardown_request(exception):
+    db=getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def show_entries():
+    cur=g.db.execute('select title, text from entries order by id desc')
+    entries=[dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_post():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) from values (?,?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash("New entry posted!")
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error=None
+    if request.method=='POST':
+        if request.form['username']!=app.config['USERNAME']:
+            error='Invalid username'
+        elif request.form['password']!=app.config['PASSWORD']:
+            error='Invalid password'
+        else:
+            session['logged_in']=True
+            flash('You are now logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login_html',error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+
+
+
+
 
 if __name__=='__main__':
     app.run(port=5010)
